5e30fd8eb3053e88f33fa4229d1b7117172212fd
Diego Lehyt <diegolehy00@gmail.com>
2022-04-16 14:56:58 -0400

    FTTH INT and TV working

--- script.js
+++ script.js
@@ -1,9 +1,237 @@
 // APP starts
 // Variables
-let modem = {
-    HomeHub2000: "HNSYM",
-    HomeHub3000: "NGMOD",
-    HomeHub4000: "4KMOD"
+let submitBtn = document.getElementById("submit");
+// let resultsBox = document.getElementById("resultsBox");
+let bundle = document.getElementById("bundle");
+let USOCS = [];
+let CS = document.getElementById("CS");
+let LST = document.getElementById("LST");
+let USOClist = document.getElementById("USOClist");
+// Indicator services
+let InternetIndicator = false;
+let TVIndicator = false;
+let HPIndicator = false;
+
+// MODEM
+let HH2K = document.getElementById("HH2K");
+let HH3K = document.getElementById("HH3K");
+let HH4K = document.getElementById("HH4K");
+
+// FTTH
+let Fibe15FTTH = document.getElementById("Fibe15FTTH");
+let Fibe25FTTH = document.getElementById("Fibe25FTTH");
+let Fibe50FTTH = document.getElementById("Fibe50FTTH");
+let Fibe150FTTH = document.getElementById("Fibe150FTTH");
+let Fibe500FTTH = document.getElementById("Fibe500FTTH");
+let Fibe1000FTTH = document.getElementById("Fibe1000FTTH");
+let Fibe1500FTTH = document.getElementById("Fibe1500FTTH");
+let Fibe3000FTTH = document.getElementById("Fibe3000FTTH");
+
+// TV
+let FibeTV = document.getElementById("FibeTV");
+let FibeTVAPP = document.getElementById("FibeTVAPP");
+let SATTV = document.getElementById("SATTV");
+
+
+// functions
+
+// SUBMIT Btn Function
+submitBtn.addEventListener("click", submit);
+
+// Generate USOC List Function
+function generateList(value) {
+        
+    let USOCelement = document.createElement("li");
+    USOCelement.className = "list-group-item"
+
+    USOCelement.innerHTML = value;
+
+    USOClist.appendChild(USOCelement);
+}
+
+function submit() {
+  // CHECK for previous Search and clear Results
+  if (USOCS != []){
+    USOCS = [];
+    USOClist.innerHTML = "";
+    InternetIndicator = false;
+    TVIndicator = false;
+    HPIndicator = false;
+  }
+
+  // S C A N S
+    // SCAN TV
+    if (FibeTV.checked == true) {
+        TVIndicator = true;
+        USOCS.push("WDATA");  
+    
+    }
+    else if (FibeTVAPP.checked == true) {
+        USOCS.push("*** Fibe TV App - NO USOC NEEDED ***"); 
+    }
+    else if (SATTV.checked == true) {
+        USOCS.push("*** Satellite TV - NO USOC NEEDED ***"); 
+    }
+    else {
+    // NO TV
+    }
+
+  // SCAN MODEM 
+  if (HH2K.checked == true) { 
+    if (TVIndicator == false){
+        USOCS.push("HNSYM (modem)");
+    } 
+    else {
+        USOCS.push("*** Fibe TV w/ HH 2000 - NO MODEM USOC NEEDED ***"); 
+    }
+  }
+  else if (HH3K.checked == true) {
+    USOCS.push("NGMOD (modem)"); 
+  }
+  else if (HH4K.checked == true) {
+    USOCS.push("4KMOD (modem)"); 
+  }
+  else { 
+    resultsBox.innerHTML = "NO MODEM USOCS NEED IT";
+  }
+
+  // SCAN INTERNET  
+  if (Fibe15FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLU19");
+    } 
+    else {
+        USOCS.push("DSLUM");
+    }    
+  }
+
+  else if (Fibe25FTTH.checked == true) {
+    InternetIndicator = true; 
+    if (TVIndicator == true){
+        USOCS.push("VLU25");
+    } 
+    else {
+        USOCS.push("DSLSN"); 
+    }  
+  }
+
+  else if (Fibe50FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLRFA");
+    } 
+    else {
+        USOCS.push("DSLFA"); 
+    }   
+  }
+  else if (Fibe150FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLRFC");
+    } 
+    else {
+        USOCS.push("DSLFC"); 
+    } 
+  }
+  else if (Fibe500FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLRFD");
+    } 
+    else {
+        USOCS.push("DSLFD"); 
+    } 
+  }
+  else if (Fibe1000FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLRFB");
+    } 
+    else {
+        USOCS.push("DSLFB"); 
+    } 
+  }
+  else if (Fibe1500FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLRFF");
+    } 
+    else {
+        USOCS.push("DSLFF"); 
+    } 
+  }
+  else if (Fibe3000FTTH.checked == true) {
+    InternetIndicator = true;
+    if (TVIndicator == true){
+        USOCS.push("VLRFX");
+    } 
+    else {
+        USOCS.push("DSLFX"); 
+    } 
+  }
+  else {
+    InternetIndicator = false  
+    resultsBox.innerHTML = "NO USOCS NEEDED";
+  }
+
+
+
+  // HP SCAN
+
+
+
+
+
+
+  // BUNDLE type
+  // INT (SINGLE)
+  if (InternetIndicator == true && TVIndicator == false && HPIndicator == false){
+
+    // MESSEGE about bundle
+    bundle.innerHTML = "INTERNET ONLY";
+
+    if (FibeTVAPP.checked == true) {
+        bundle.innerHTML = "INTERNET / Fibe TV App - DUO";
+    }
+    else if (SATTV.checked == true) {
+        bundle.innerHTML = "INTERNET / Satellite TV - DUO";
+    }
+
+    // PRINT INFORMATION and send to USOC array
+
+    LST.innerHTML = "LST: DERXX"
+    CS.innerHTML = "CS: DRYXX"
+
+    // pushing USOCs
+    USOCS.push("FTTHR (Fibre to the Home)");
+    USOCS.push("1RDDL (Main dry loop)");
+    USOCS.push("RDSLC (DSL access w/o HP)");  
+     
+    
+    // Generate List
+    USOCS.forEach(generateList);
+
+  }
+  
+  // INT + TV (DUO)
+  if (InternetIndicator == true && TVIndicator == true && HPIndicator == false){
+
+    bundle.innerHTML = "INTERNET / Fibe TV - DUO";
+
+    CS.innerHTML = "CS: VLRXX"
+    LST.innerHTML = "LST: DERXX"
+
+    // pushing USOCs
+    USOCS.push("FTTHR (Fibre to the Home)");
+    USOCS.push("1RDDL (Main dry loop)");
+    USOCS.push("WDATA (DSL with Fibe TV)");  
+     
+    
+    // Generate List
+    USOCS.forEach(generateList);
+
+  }
 }
 
 // APP ends
